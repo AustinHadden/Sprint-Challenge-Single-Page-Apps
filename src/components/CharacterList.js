a9c1feb393979e63db6c0cf0c6bcff0ed395208e
AustinHadden <vitovalletori@gmail.com>
2019-10-18 13:23:27 -0500

    finally got search working, thank you Brandon

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,15 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import CharacterCard from "./CharacterCard";
-import styled from "styled-components";
 import SearchForm from "./SearchForm";
 
-const StyledCard = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;    
-    margin: 1%;   
-`;
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -20,19 +12,14 @@ export default function CharacterList() {
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios.get(`https://rickandmortyapi.com/api/character/`)
     .then(response => {
-        console.log(response);
+        //console.log(response);
         setCharacters(response.data.results);
     })
   }, []);
 
   return (
     <div>
-    <SearchForm characters={characters} />
-    <StyledCard>
-      {characters.map(character => (
-        <CharacterCard key={character.id} name={character.name} img={character.image} species={character.species} status={character.status} />
-      ))}
-    </StyledCard>    
+    <SearchForm  characters={characters}/>
     </div>
   );
 }
